81780ab984b511e6e9059cdde50ddb88dbe8f8fb
valweb-ux <valweb@gmail.com>
2025-03-09 17:22:18 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Змінено 'hybrid' на 'standalone', оскільки 'hybrid' не підтримується
-  output: 'standalone',
+  output: 'export',
   images: {
-    domains: ['localhost'],
+    unoptimized: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  }
 };
 
 export default nextConfig;
-
